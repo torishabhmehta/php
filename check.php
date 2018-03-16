0734dc2bd214a9222f8c5b8d55217a6254622584
First Name <you@example.com>
2018-03-17 05:19:42 +0530

    first commit

--- check.php
+++ check.php
@@ -8,21 +8,20 @@ mysql_connect("192.168.121.187","first_year","first_year");
 mysql_select_db("first_year_db");
 if(isset($_POST['urname']))
 {
-   $username=$_POST['username'];
+   $username=$_POST['urname'];
 
-    $checkdata=" SELECT username FROM rishabh_profile WHERE username='$username' ";
+   $checkdata=" SELECT username FROM rishabh_profile WHERE username='$username' ";
 
-     $query=mysql_query($checkdata);
+   $query=mysql_query($checkdata);
 
-      if(mysql_num_rows($query)>0)
-         {
-             echo "OK";
-              }
-       else
-          {
-              echo "OK";
-               }
-        exit();
+   if(mysql_num_rows($query)>0)
+   {
+     echo "Not OK";
+   }
+   else
+   {
+     echo "OK";
+   }
+   exit();
 }
-     
 ?>
